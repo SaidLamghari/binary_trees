912245862611a6653470e659169e746c43061e84
SaidLamghari <said.lmg@gmail.com>
2024-02-28 07:45:03 -0800

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,28 +1,31 @@
+#include <stdlib.h>
 #include "binary_trees.h"
-
 /**
  * binary_tree_node - Function that creates a B.T. node.
  *
  * @parent: Pointer to the parent node .
+ *
  * @value: Value of the new node.
  *
  * Return: the New node or NULL.
- **/
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *nbtnode;
+	binary_tree_t *newBTnode;
 
-	nbtnode = malloc(sizeof(binary_tree_t));
-	if (nbtnode == NULL)
+	newBTnode = malloc(sizeof(binary_tree_t));
+	if (newBTnode == NULL)
 	{
-		return (NULL);
+	return (NULL);
 	}
 	else
 	{
-		nbtnode->n = value;
-		nbtnode->parent = parent;
-		nbtnode->left = NULL;
-		nbtnode->right = NULL;
-		return (nbtnode);
+		newBTnode->n = value;
+		newBTnode->parent = parent;
+		newBTnode->right = NULL;
+		newBTnode->left = NULL;
 	}
+
+	return (newBTnode);
 }
+
