57faf77dbc50d2465eaae11369c657d51320a238
SaidLamghari <said.lmg@gmail.com>
2024-02-28 08:41:00 -0800

    binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -25,7 +25,6 @@ typedef struct binary_tree_s binary_tree_t;
 
 
 void binary_tree_print(const binary_tree_t *);
-typedef struct binary_tree_s binary_tree_t;
 
 typedef struct binary_tree_s bst_t;
 
