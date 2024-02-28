464d74fb91da90887ce7485bda473275b5f7ce1f
SaidLamghari <said.lmg@gmail.com>
2024-02-28 07:46:07 -0800

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -14,6 +14,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *newBTnode;
 
 	newBTnode = malloc(sizeof(binary_tree_t));
+
 	if (newBTnode == NULL)
 	{
 	return (NULL);
