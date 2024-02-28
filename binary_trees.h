686f1512ca5fb80b64f068b0c5e6c9d1c3d2319b
SaidLamghari <said.lmg@gmail.com>
2024-02-28 11:01:52 -0800

    5-binary_tree_is_root.c

--- binary_trees.h
+++ binary_trees.h
@@ -34,7 +34,7 @@ typedef struct binary_tree_s avl_t;
 
 
 
-nt binary_tree_is_root(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
 
 int binary_tree_is_leaf(const binary_tree_t *node);
 
