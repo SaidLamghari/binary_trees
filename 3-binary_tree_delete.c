f5f3ac67f00d8c269074f2e6278de2537fa133a7
SaidLamghari <said.lmg@gmail.com>
2024-02-28 10:08:26 -0800

    2-binary_tree_insert_right.c

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_delete - Start of funcnction that
+ * Deletes an entire binary tree
+ * @tree: Ptr of the root that will be deleted
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return;
+
+	}
+	else
+	{
+
+
+
+		binary_tree_delete(tree->left);
+
+
+		binary_tree_delete(tree->right);
+
+
+		free(tree);
+	}
+
+
+
+	tree = NULL;
+
+
+
+}
