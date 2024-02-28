7c073f3bed76ee78e074ac5b897c8e1a8883e0bc
SaidLamghari <said.lmg@gmail.com>
2024-02-28 05:50:55 -0800

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -16,13 +16,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
-	else
-	{
 		nbtnode->n = value;
 		nbtnode->parent = parent;
 		nbtnode->right = NULL;
 		nbtnode->left = NULL;
 
 		return (nbtnode);
-	}
 }
