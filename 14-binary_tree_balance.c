11f43bd4a65f68b319cb92c24ea00ed1ca444924
SaidLamghari <said.lmg@gmail.com>
2024-02-29 03:07:20 -0800

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -58,13 +58,16 @@ int binary_tree_balance(const binary_tree_t *tree)
 	{
 		return (0);
 	}
+	else
+	{
+		rightHght = binary_tree_heightc(tree->right);
 
 
 		leftHght = binary_tree_heightc(tree->left);
 
-		rightHght = binary_tree_heightc(tree->right);
 
 		rslt = leftHght - rightHght;
+	}
 
 		return (rslt);
 
