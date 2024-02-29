f71ed964b9ab140b1e5f4a85325f402179953f72
SaidLamghari <said.lmg@gmail.com>
2024-02-29 06:49:07 -0800

    16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,32 +1,43 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_is_perfect - Start of function that checks
+ * binary_tree_is_perfect - Statrt of function that Checks
  * if a binary tree is perfect
  * @tree: the pointer to the root
+ *
  * Return: 1 or 0
- * */
+ */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t valExpctd;
-	size_t valHght;
+	int valPLeft;
+	int valPRight;
+	int valLHeight;
+	int valRheight;
 
-	valExpctd = 0;
-	valHght = 0;
-	    
+	valPLeft = 0;
+	valPRight = 0;
+	valLHeight = 0;
+	valRheight = 0;
+
 	if (tree == NULL)
 	{
+		return (0);
+	}
 
+
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+
+	if (tree->left == NULL || tree->right == NULL)
 		return (0);
 
-	}
-	        
+
+	valPLeft = binary_tree_is_perfect(tree->left);
+	valPRight = binary_tree_is_perfect(tree->right);
+	valLHeight = binary_tree_height(tree->left);
+	valRheight = binary_tree_height(tree->right);
 
 
-	valHght = binary_tree_height(tree);
-
-
-	valExpctd = (1 << valHght) - 1;
-
-	return (binary_tree_size(tree) == valExpctd);
+	return (valPLeft && valPRight && valLHeight == valRheight);
 }
